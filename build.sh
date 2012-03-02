f326e571a00cbce1116f7f2baafa887387fa3eaf
Peter Kröner <peter@peterkroener.de>
2012-03-03 00:00:37 +0100

    Remote control basics work
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cd lib
 docco controls.js frame.js presenter.js presentation.js worker.js pik.js print.js
 cd ../plugins
 docco backgroundHarmonizer.js inputProtector.js printHandout.js
+cd ../remote
+docco remote.js
---
+++ remote/remote.js
@@ -0,0 +1,57 @@
+// The **Remote** is a Node.js program that works exactly like the Web Worker that enables
+// synchronized presentations in multiple browser windows. The difference is, that the
+// remote uses the network to synchronize the presentation, which means that you can use
+// multiple browsers or even mutiple devices to control your slides. Want to use your
+// iPhone as a remote control? Start this script, open the presentation on both your
+// presentation computer and your phone, use the "Remote" link in the control and enter
+// the remote server's ip address.
+
+/*jshint node:true, globalstrict:true */
+
+"use strict";
+
+// Setup Socket.io
+var io = require('socket.io').listen(16911);
+
+// The remote caches a copy of the presentation state. Its properties are:
+//
+// * `initialized`: True once the remote has been initialized by a presentation instance
+// * `file`: Path to the current presentation
+// * `slide`: Current slide number
+// * `hide`: True if the presentation is hidden at the moment
+var cached_state = {
+	initialized: false,
+	file: 'start.html',
+	slide: 0,
+	hide: ''
+};
+
+// The list of connected presentation instances.
+var clients = [];
+
+// On conenction, post the cached state and setup the event to recieve states from the
+// presentation
+io.sockets.on('connection', function(socket){
+	clients.push(socket);
+	socket.on('message', handleIncomingState);
+	socket.emit('message', cached_state);
+});
+
+// Process an incoming state from one of the presentation instances. Merge the incoming
+// state with the current state and, if there was any new information, broadcast the
+// result to all connected presentations except the specified client.
+var handleIncomingState = function(data, source){
+	Object.keys(data).forEach(function(key){
+		cached_state[key] = data[key];
+	});
+	broadcastCurrentState(source);
+};
+
+// Broadcast the cached state to all presentation instances except the specified client.
+var broadcastCurrentState = function(source){
+	for(var i = 0, l = clients.length; i < l; i++){
+		if(clients[i] && clients[i] !== source){
+			clients[i].emit('message', cached_state);
+		}
+	}
+};
